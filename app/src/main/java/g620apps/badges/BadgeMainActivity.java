416662aadd58039dc80e050a3749e2a239c2fc22
LeJacque Gillespie <m0321us@yahoo.com>
2016-07-04 11:15:58 -0500

    Initial commit

---
+++ app/src/main/java/g620apps/badges/BadgeMainActivity.java
@@ -0,0 +1,16 @@
+package g620apps.badges;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BadgeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badge_main);
+
+    }
+}
+
+//add appropriate pics to the mipmap folder and remove the green monsters.  
